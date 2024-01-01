7bdb35a983a8ca135acb15f02dc0ea62054f9ee1
shashankpatil125 <shashankpatil125@gmail.com>
2024-01-01 17:44:26 +0530

    fix: minor fixes at header

--- components/Header/DropdownUser.tsx
+++ components/Header/DropdownUser.tsx
@@ -112,7 +112,7 @@ const DropdownUser = () => {
           </li>
           <li>
             <Link
-              href="#"
+              href="/showcase"
               className="flex items-center gap-3.5 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
             >
               <svg
@@ -128,7 +128,7 @@ const DropdownUser = () => {
                   fill=""
                 />
               </svg>
-              My Contacts
+              My Certificates
             </Link>
           </li>
           <li>
